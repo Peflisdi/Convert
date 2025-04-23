442e8ebfc098daa5692e40257887051af2dd6361
peflis <peflisdi@gmail.com>
2025-04-23 17:11:38 -0300

    finishing project

--- script.js
+++ script.js
@@ -1,7 +1,61 @@
+const USD = 4.87
+const EUR = 5.32
+const JPY = 0.039
+
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
+const result = document.getElementById("result")
 
 amount.addEventListener("input", () => {
   
   const hasCharactersRegex = /\D+/g
   amount.value = amount.value.replace(hasCharactersRegex, "")
 })
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  switch(currency.value){
+    case "USD":
+      convertCurrency(amount.value, USD, "US$")
+      break
+      case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break
+      case "JPY":
+      convertCurrency(amount.value, JPY, "¥")
+      break
+  }
+}
+
+function convertCurrency(amount, price, symbol) {
+  try {
+    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
+
+    let total = amount * price
+
+    if(isNaN(total)){
+      return alert("Por favor, digite o valor corretamente")
+    }
+
+    total = formatCurrencyBRL(total).replace("R$", "")
+
+    result.textContent = `${total} Reais`
+
+    footer.classList.add("show-result")
+  } catch (error) {
+    footer.classList.remove("show-result")
+    console.log(error)
+    alert("Não foi possível converter, tente novamente mais tarde.")
+  }
+}
+
+function formatCurrencyBRL(value) {
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })
+}
